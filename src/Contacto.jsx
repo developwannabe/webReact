517b40124fcc65bbbfc9c91d0f4c2b585f933034
illan.martinez@hotmail.com <illan.martinez@hotmail.com>
2023-03-14 21:12:35 +0100

    Prototipo portada terminado

--- src/Contacto.jsx
+++ src/Contacto.jsx
@@ -1,6 +1,3 @@
-import Navi from './Navi.jsx';
-import Footer from './Footer.jsx';
-import { Routes, Route } from 'react-router-dom';
 
 export default function Contacto(){
     return(
--- src/Inicio.jsx
+++ src/Inicio.jsx
@@ -1,14 +1,17 @@
 import './App.css';
+import { Link } from "react-router-dom"
+import bingus from "./images/bingus.jpg"
 
 export default function Inicio() {
   return (
     <>
-      <div class="hero min-h-fit-content bg-base-200">
-        <div class="hero-content text-center">
-          <div class="max-w-md">
-            <h1 class="text-5xl font-bold">Hello there</h1>
-            <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-            <button class="btn btn-primary">Get Started</button>
+      <div className="hero bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img src={bingus} className="max-w-sm rounded-lg shadow-2xl"/>
+          <div>
+            <h1 className="text-5xl font-bold">¡Hola, soy Illán!</h1>
+            <p className="py-6">He creado esta web para que puedas conocerme un poco mejor. Puedes consultar mis proyectos, CV y maneras de contactarme.</p>
+            <Link to="/cv"><button className="btn btn-primary">Ver CV</button></Link>
           </div>
         </div>
       </div>
