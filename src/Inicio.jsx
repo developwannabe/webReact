b0938ee19daa877288ae0ff987759d1348f3fb84
developwannabe <illan.martinez@hotmail.com>
2023-05-18 15:18:27 +0200

    asd

--- src/Inicio.jsx
+++ src/Inicio.jsx
@@ -9,7 +9,7 @@ export default function Inicio() {
         <div className="hero-content flex-col lg:flex-row-reverse">
           <img src={bingus} className="max-w-sm rounded-lg shadow-2xl"/>
           <div>
-            <h1 className="text-5xl font-bold">¡Hola, soy Illán!</h1>
+            <h1 className="text-5xl font-bold">¡¡Hola, soy Illán!!</h1>
             <p className="py-6">He creado esta web para que puedas conocerme un poco mejor. Puedes consultar mis proyectos, CV y maneras de contactarme.</p>
             <Link to="/cv"><button className="btn btn-primary">Ver CV</button></Link>
           </div>
